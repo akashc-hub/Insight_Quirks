6f704026fa3d00f016bba9ecc4a922e5685bfb84
Abhishek Nikam <79035081+Abhisheknik@users.noreply.github.com>
2023-10-08 20:30:09 +0530

    Abhishek commit message here

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 ScrollReveal({ reset: true });
 
 ScrollReveal().reveal(".show-once", {
-    reset: false
+  reset: false
 });
 
 
 ScrollReveal().reveal(".fade-in", {
-    duration: 3000,
-    origin: "bottom",
-    distance: "200px",
-    easing: "cubic-bezier(0.5, 0, 0, 1)",
+  duration: 3000,
+  origin: "bottom",
+  distance: "200px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
 
-    scale:0.4,
-    rotate: {
-        x: 20,
-        z: -10
-    }
-    
+  scale: 0.4,
+  rotate: {
+    x: 20,
+    z: -10
+  }
+
 });
 
 
@@ -44,21 +44,21 @@ function transforms(x, y, el) {
   let box = el.getBoundingClientRect();
   let calcX = -(y - box.y - (box.height / 2)) / constrain;
   let calcY = (x - box.x - (box.width / 2)) / constrain;
-  
+
   return "perspective(100px) "
-    + "   rotateX("+ calcX +"deg) "
-    + "   rotateY("+ calcY +"deg) ";
+    + "   rotateX(" + calcX + "deg) "
+    + "   rotateY(" + calcY + "deg) ";
 };
 
- function transformElement(el, xyEl) {
-  el.style.transform  = transforms.apply(null, xyEl);
+function transformElement(el, xyEl) {
+  el.style.transform = transforms.apply(null, xyEl);
 }
 
-mouseOverContainer.onmousemove = function(e) {
+mouseOverContainer.onmousemove = function (e) {
   let xy = [e.clientX, e.clientY];
   let position = xy.concat([ex1Layer]);
 
-  window.requestAnimationFrame(function(){
+  window.requestAnimationFrame(function () {
     transformElement(ex1Layer, position);
   });
 };
@@ -94,7 +94,7 @@ function toggleMode() {
 // Event listener for the mode toggle button
 modeToggle.addEventListener('click', toggleMode);
 
- 
+
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 25,
@@ -112,15 +112,110 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 
-  breakpoints:{
-      0: {
-          slidesPerView: 1,
-      },
-      520: {
-          slidesPerView: 2,
-      },
-      950: {
-          slidesPerView: 3,
-      },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
   },
+});
+
+
+var navLink = gsap.utils.toArray(".features-title3"),
+  imgWrap = document.querySelector(".img-wrapper"),
+  imgItem = document.querySelector(".img-placeholder img");
+
+
+function moveImg(e) {
+  var mouseX = e.clientX,
+    mouseY = e.clientY
+  t1 = gsap.timeline();
+  t1.to(imgWrap, {
+    duration: 1,
+    x: mouseX,
+    y: mouseY,
+    ease: Expo.ease
+  })
+}
+
+
+function linkHover(e) {
+  if (e.type === "mouseenter") {
+    var imgSrc = e.target.dataset.src;
+    var t1 = gsap.timeline();
+
+
+    t1.set(imgItem, {
+      attr: { src: imgSrc }
+    }).to(imgWrap, {
+      autoAlpha: 1,
+      scale: 2
+    });
+  } else if (e.type === "mouseleave") {
+    var t1 = gsap.timeline();
+    t1.to(imgWrap, {
+      autoAlpha: 0,
+      scale: 0.2
+    })
+  }
+
+}
+
+
+function initAnimation() {
+  navLink.forEach(link => {
+    link.addEventListener('mouseenter', linkHover);
+    link.addEventListener('mouseleave', linkHover);
+    link.addEventListener('mousemove', moveImg);
+  })
+}
+
+function init() {
+  initAnimation();
+}
+
+window.addEventListener("load", function () {
+  init();
+})
+
+var cursor = document.querySelector('.cursor'),
+    cursorScale = document.querySelectorAll('.cursor-scale'),
+    mouseX = 0,
+    mouseY = 0
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        })
+    }
+});
+
+window.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY
+});
+
+cursorScale.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        cursor.classList.remove('grow');
+        cursor.classList.remove('grow-small');
+    });
+    link.addEventListener('mousemove', () => {
+        cursor.classList.add('grow');
+        if(link.classList.contains('small')){
+            cursor.classList.remove('grow');
+            cursor.classList.add('grow-small');
+        }
+    });
 });
